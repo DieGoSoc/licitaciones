2c476a1753b7a08d80d238858987448cec002bfb
Diego <diegotr@gmail.com>
2023-10-22 19:37:55 +0100

    dictionary step

--- src/_01_extract_data.py
+++ src/_01_extract_data.py
@@ -1,22 +1,38 @@
-import polars 
 import xml.etree.ElementTree as ET
 import os
 from tqdm import tqdm
+import polars as pl
 
 def get_file_info (path = r'C:\Users\diego\Desktop\licitaciones\data\licitacionesPerfilesContratanteCompleto3_20201222_192258.atom'):
+    namespaces = {
+        'cbc-place-ext':"urn:dgpe:names:draft:codice-place-ext:schema:xsd:CommonBasicComponents-2",
+        'cac-place-ext':"urn:dgpe:names:draft:codice-place-ext:schema:xsd:CommonAggregateComponents-2",
+        'cbc':"urn:dgpe:names:draft:codice:schema:xsd:CommonBasicComponents-2",
+        'cac':"urn:dgpe:names:draft:codice:schema:xsd:CommonAggregateComponents-2",
+        'ns1':"urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"
+    }
     tree = ET.parse(path)
     root = tree.getroot()
-    print(root)
-    for child in root:
-        if child.tag == '{http://www.w3.org/2005/Atom}entry':
-            print()
-        print(child.tag, child.attrib)
+    entries = {}
+    for entry in root.findall('{http://www.w3.org/2005/Atom}entry'):
+        postcode = entry.find(".//cbc:PostalZone", namespaces).text
+        title = entry.find('{http://www.w3.org/2005/Atom}title', namespaces).text
+        id = entry.find('{http://www.w3.org/2005/Atom}id', namespaces).text
+        #id number extraction
+        entries [id] = {'title' : title, 'postcode' : postcode}
+    return entries
 
 def extract_data (folder = r'C:\Users\diego\Desktop\licitaciones\data'):
     files = os.listdir(folder)
-    progress = tqdm(files)
+    progress = tqdm(files[:10])
+    data = []
+    df = pd.DataFrame()
     for f in progress:
         fpath = os.path.join(folder,f)
-        get_file_info(fpath)
-    print('hi')
+        titles = get_file_info(fpath)
+        data.append(titles)
+    df['titles'] = data
+    return df
+
+
 
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from _01_extract_data import extract_data
+from _01_extract_data import get_file_info
+from pandas import pandas as pd
 
 if __name__ == '__main__':
     print(extract_data())
