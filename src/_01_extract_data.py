c2b049a12066208d106c2ad53f6a543a39aa7d28
Diego <diegotr@gmail.com>
2023-11-13 19:40:26 +0000

    Update extract_data

--- src/_01_extract_data.py
+++ src/_01_extract_data.py
@@ -1,38 +1,46 @@
-import xml.etree.ElementTree as ET
 import os
-from tqdm import tqdm
-import polars as pl
+import xml.etree.ElementTree as ET
 
-def get_file_info (path = r'C:\Users\diego\Desktop\licitaciones\data\licitacionesPerfilesContratanteCompleto3_20201222_192258.atom'):
+import polars as pl
+from tqdm import tqdm
+
+
+def get_file_info(
+    path=r'C:\Users\diego\Desktop\licitaciones\data\licitacionesPerfilesContratanteCompleto3_20201222_192258.atom'
+):
     namespaces = {
-        'cbc-place-ext':"urn:dgpe:names:draft:codice-place-ext:schema:xsd:CommonBasicComponents-2",
-        'cac-place-ext':"urn:dgpe:names:draft:codice-place-ext:schema:xsd:CommonAggregateComponents-2",
-        'cbc':"urn:dgpe:names:draft:codice:schema:xsd:CommonBasicComponents-2",
-        'cac':"urn:dgpe:names:draft:codice:schema:xsd:CommonAggregateComponents-2",
-        'ns1':"urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"
+        'cbc-place-ext': 'urn:dgpe:names:draft:codice-place-ext:schema:xsd:CommonBasicComponents-2',
+        'cac-place-ext': 'urn:dgpe:names:draft:codice-place-ext:schema:xsd:CommonAggregateComponents-2',
+        'cbc': 'urn:dgpe:names:draft:codice:schema:xsd:CommonBasicComponents-2',
+        'cac': 'urn:dgpe:names:draft:codice:schema:xsd:CommonAggregateComponents-2',
+        'ns1': 'urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2',
     }
     tree = ET.parse(path)
     root = tree.getroot()
     entries = {}
     for entry in root.findall('{http://www.w3.org/2005/Atom}entry'):
-        postcode = entry.find(".//cbc:PostalZone", namespaces).text
+        postcode = entry.find('.//cbc:PostalZone', namespaces).text
         title = entry.find('{http://www.w3.org/2005/Atom}title', namespaces).text
         id = entry.find('{http://www.w3.org/2005/Atom}id', namespaces).text
-        #id number extraction
-        entries [id] = {'title' : title, 'postcode' : postcode}
+        # id number extraction
+        id = id.replace('https://contrataciondelestado.es/sindicacion/licitacionesPerfilContratante/', '')
+        if id in entries:
+            continue  # there are some idetical values
+        entries[id] = {'title': title, 'postcode': postcode}
     return entries
 
-def extract_data (folder = r'C:\Users\diego\Desktop\licitaciones\data'):
+
+def extract_data(folder=r'C:\Users\diego\Desktop\licitaciones\data'):
     files = os.listdir(folder)
-    progress = tqdm(files[:10])
-    data = []
-    df = pd.DataFrame()
+    progress = tqdm(files[:10])  # just first 10 only to debug
+    data = {}
     for f in progress:
-        fpath = os.path.join(folder,f)
-        titles = get_file_info(fpath)
-        data.append(titles)
-    df['titles'] = data
+        fpath = os.path.join(folder, f)
+        entries = get_file_info(fpath)
+        data = {**data, **entries}
+    new_list = []
+    for k, v in data.items():
+        v['id'] = k
+        new_list.append(v)
+    df = pl.DataFrame(new_list, orient='row')
     return df
-
-
-
